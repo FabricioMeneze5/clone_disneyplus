9e1f57bc8e84a54d7d39126cb0e46d4b85df1bfa
Fabricio Menezes <fabricio-menezes@live.com>
2024-03-31 19:29:13 +0100

    add section FAQ

--- src/main.js
+++ src/main.js
@@ -1,17 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const question = document.querySelectorAll('[data-faq-question]');
 
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(clickedButton) {
             const tabTarget = clickedButton.target.dataset.tabButton;
             const tab = document.querySelector(`[data-tab-id=${tabTarget}]`);
+
             hideAllTabs();
             tab.classList.add('shows__list--is-active');
             removeActiveButton()
             clickedButton.target.classList.add('shows__tabs__button--is-active');
         })
     }
+
+    for (let i = 0; i < question.length; i++) {
+        question[i].addEventListener('click', openOrClose);
+    }
 })
+
+function openOrClose(element) {
+    const classe = 'faq__questions__item--is-open';
+    const elementFather = element.target.parentNode;
+
+    elementFather.classList.toggle(classe);
+}
 
 function removeActiveButton() {
     const buttons = document.querySelectorAll('[data-tab-button]');
